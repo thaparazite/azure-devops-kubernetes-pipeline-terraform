e1ddb637bfcc960149bf7e14a8592f35531f9a20
Flaviu Vanca <skety_tm@yahoo.com>
2025-06-24 15:54:07 +0100

    commit

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -1,65 +1,64 @@
 # aws --version
-# aws eks --region us-east-1 update-kubeconfig --name in28minutes-cluster
-# Uses default VPC and Subnet. Create Your Own VPC and Private Subnets for Prod Usage.
-# terraform-backend-state-in28minutes-123
-# AKIA4AHVNOD7OOO6T4KI
-
+# aws eks --region eu-west-1 update-kubeconfig --name sketysoft-cluster
+# Uses default VPC and Subnet. For production, create your own VPC and private subnets.
+# terraform-backend-state-sketysoft
+# (Remove any sensitive keys from code and use environment variables or secret managers)
 
 terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws        = { source = "hashicorp/aws", version = ">= 5.0" }
+    kubernetes = { source = "hashicorp/kubernetes", version = ">= 2.25" }
+  }
   backend "s3" {
     bucket = "mybucket" # Will be overridden from build
     key    = "path/to/my/key" # Will be overridden from build
-    region = "us-east-1"
+    region = "eu-west-1"
   }
 }
 
-resource "aws_default_vpc" "default" {
-
+provider "aws" {
+  region = "eu-west-1"
 }
+
+resource "aws_default_vpc" "default" {}
 
 data "aws_subnet_ids" "subnets" {
   vpc_id = aws_default_vpc.default.id
 }
 
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-  version                = "~> 2.12"
-}
-
-module "in28minutes-cluster" {
+module "sketysoft-cluster" {
   source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = "in28minutes-cluster"
-  cluster_version = "1.14"
-  subnets         = ["subnet-3f7b2563", "subnet-4a7d6a45"] #CHANGE
-  #subnets = data.aws_subnet_ids.subnets.ids
+  version         = "~> 20.0"
+  cluster_name    = "sketysoft-cluster"
+  cluster_version = "1.29"
+  subnets         = data.aws_subnet_ids.subnets.ids
   vpc_id          = aws_default_vpc.default.id
 
-  #vpc_id         = "vpc-1234556abcdef"
-
-  node_groups = [
-    {
-      instance_type = "t2.micro"
-      max_capacity  = 5
-      desired_capacity = 3
-      min_capacity  = 3
+  eks_managed_node_groups = {
+    default = {
+      instance_types = ["t3.small"]
+      min_size       = 2
+      max_size       = 5
+      desired_size   = 3
     }
-  ]
+  }
 }
 
 data "aws_eks_cluster" "cluster" {
-  name = module.in28minutes-cluster.cluster_id
+  name = module.sketysoft-cluster.cluster_name
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-  name = module.in28minutes-cluster.cluster_id
+  name = module.sketysoft-cluster.cluster_name
 }
 
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
 
-# We will use ServiceAccount to connect to K8S Cluster in CI/CD mode
-# ServiceAccount needs permissions to create deployments 
-# and services in default namespace
 resource "kubernetes_cluster_role_binding" "example" {
   metadata {
     name = "fabric8-rbac"
@@ -74,9 +73,4 @@ resource "kubernetes_cluster_role_binding" "example" {
     name      = "default"
     namespace = "default"
   }
-}
-
-# Needed to set the default region
-provider "aws" {
-  region  = "us-east-1"
 }
